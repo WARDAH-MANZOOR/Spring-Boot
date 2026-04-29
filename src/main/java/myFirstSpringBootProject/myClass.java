dcbe39da871d99a69be9c62fc2a36cb5c718f03c
WARDAH-MANZOOR <wardahmanzoor0467@gmail.com>
2026-04-29 10:25:03 +0500

    added controllers by @RestController and routes by @GetMapping

--- src/main/java/myFirstSpringBootProject/myClass.java
+++ src/main/java/myFirstSpringBootProject/myClass.java
@@ -3,10 +3,26 @@ package myFirstSpringBootProject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.time.LocalDate;
+
+@RestController    // API class banata hai
 public class myClass {
-    @GetMapping("abc")
+
+    @GetMapping("hello")    // URL define karta hai
     public String sayHello(){
+
         return "Hello World";
+    }
+
+    @GetMapping("myname")    // URL define karta hai
+    public String myName(){
+
+        return "My name is Wardah";
+    }
+
+    @GetMapping("date")    // URL define karta hai
+    public String date(){
+
+        return LocalDate.now().toString();
     }
 }
